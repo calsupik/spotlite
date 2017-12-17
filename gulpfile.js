d29b498a129441377763a43c726e065b1f6c817f
Cal Supik <calsupik@gmail.com>
2017-12-17 16:03:12 -0500

    Update public folder and use cloudflare for third party scripts

--- gulpfile.js
+++ gulpfile.js
@@ -13,12 +13,12 @@ gulp.task('backend', function() {
 
 //Global Files
 var indexHTML = './www/index.html';
-var indexJS = './web/js/index.js';
+var indexJS = './web/public/js/index.js';
 
 //Replace HTML with dev HTML
 gulp.task('dev-html', function() {
   gulp.src(indexHTML)
-    .pipe(replace('<script src="https://spot-lite.herokuapp.com/js/index.js"></script>', '<script src="../web/js/index.js"></script>'))
+    .pipe(replace('https://spot-lite.herokuapp.com/','../web/public/'))
     .pipe(replace('<script src="cordova.js"></script>', '<!--<script src="cordova.js"> </script>-->'))
     .pipe(gulp.dest('./www/'))
 });
@@ -27,13 +27,13 @@ gulp.task('dev-html', function() {
 gulp.task('dev-js', function() {
   gulp.src(indexJS)
     .pipe(replace('document.addEventListener(\'deviceready\', app.onDeviceReady, false);', 'app.onDeviceReady();'))
-    .pipe(gulp.dest('./web/js/'))
+    .pipe(gulp.dest('./web/public/js/'))
 });
 
 //Replace HTML with prod HTML
 gulp.task('prod-html', function() {
   gulp.src(indexHTML)
-    .pipe(replace('<script src="../web/js/index.js"></script>', '<script src="https://spot-lite.herokuapp.com/js/index.js"></script>'))
+    .pipe(replace('../web/public/','https://spot-lite.herokuapp.com/'))
     .pipe(replace('<!--<script src="cordova.js"> </script>-->', '<script src="cordova.js"></script>'))
     .pipe(gulp.dest('./www/'))
 });
@@ -42,7 +42,7 @@ gulp.task('prod-html', function() {
 gulp.task('prod-js', function() {
   gulp.src(indexJS)
     .pipe(replace('app.onDeviceReady();', 'document.addEventListener(\'deviceready\', app.onDeviceReady, false);'))
-    .pipe(gulp.dest('./web/js/'))
+    .pipe(gulp.dest('./web/public/js/'))
 });
 
 //Run dev tasks
